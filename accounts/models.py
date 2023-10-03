1f746a6035ebfebe13c7b87911945ed4f89b3984
Amr Gebil <amrgebil1@gmail.com>
2023-10-03 19:38:59 +0300

    I created my account manager, account, and profile models, and I configured the configurations of media files and the admin panel.

---
+++ accounts/models.py
@@ -0,0 +1,82 @@
+
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+
+# Create your models here.
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self,frist_name,last_name,username,email,password=None):
+        if not email:
+            raise  ValueError('user must have email address')
+        if not username:
+            raise  ValueError('user must have username')
+        user =self.model(
+            email=self.normalize_email(email),
+            username=username,
+            frist_name=frist_name,
+            last_name=last_name
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self,frist_name,last_name,username,email,password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+            frist_name=frist_name,
+            last_name=last_name
+        )
+        user.is_admin=True
+        user.is_admin = True
+        user.is_staff = True
+        user.is_active = True
+        user.is_superadmin = True
+
+        user.save(using=self._db)
+        return user
+
+
+
+class Account(AbstractBaseUser):
+    frist_name =models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username= models.CharField(max_length=50,unique=True)
+    email = models.EmailField(max_length=100,unique=True)
+    phone_number = models.CharField(max_length=50)
+
+    #required
+    data_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username','frist_name','last_name']
+
+    objects=MyAccountManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self,perm,obj=None):
+        return self.is_admin
+
+    def has_module_perms(self,add_label):
+        return True
+
+
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='accounts/profile', blank=True)
+    bio = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.user.email
+
--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import  os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
@@ -116,8 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+ #Media
+MEDIA_ROOT =os.path.join(BASE_DIR,'media')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.Account'
